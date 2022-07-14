f41171eb0537b94e7a092d9323748e7a248f0c72
Go3shom <shadi.muhammad001@gmail.com>
2022-07-15 00:46:37 +0200

    Create ProductController & Worked on Show, Edit, Update, Delete Functionality

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return view('products.index', [
+            'products' => $products,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('products.show', [
+            'product' => Product::findOrFail($id)
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('products.edit', [
+            'product' => Product::findOrFail($id)
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function update(Request $request, $id)
+    public function update(Product $product, Request $request)
+    {
+        $validatedProduct = $request->validate([
+            'name' => ['required', 'min:3', 'max:255'],
+            'price' => ['required', 'numeric'],
+            'weight' => ['required', 'numeric'],
+            'quantity' => ['required', 'numeric'],
+        ]);
+
+        $product->update($validatedProduct);
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Product::findOrFail($id)->delete();
+
+        return redirect()->route('products.index');
+    }
+}
