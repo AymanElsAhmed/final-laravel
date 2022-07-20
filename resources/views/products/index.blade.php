76eaf3f61e743e62c8f8b3cf30aea7dc98d0c0bb
Ayman Elsayed <ayman.elsayed628@gmail.com>
2022-07-20 10:25:37 +0200

    done with product controller

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,9 +12,8 @@
         <label for="search" class="form-label h2 text-primary">البحث</label>
         <input type="text" class="form-control" id="search" aria-describedby="search"  name="search">
         <div id="search" class="form-text">ابحث من خلال العنوان </div>
-      </div>
+    </div>
     
-
 </form>
     
 <div class="row mb-3 mt-3 g-5 " dir="rtl">
@@ -60,51 +59,5 @@
     </div>
     @endif
 </div>
-    {{-- <div class="container">
-        @if ($products->count() == 0)
-            <h3>{{ __('No Products Yet') }}</h3>
-        @else
-            <table border="2" cellpadding="5">
-                <thead align="center">
-                    <tr>
-                        <th>{{ __('Product') }}</th>
-                        <th>{{ __('Price') }}</th>
-                        <th>{{ __('Weight') }}</th>
-                        <th>{{ __('Quantity') }}</th>
-                        <th>{{ __('User') }}</th>
-                        <th colspan="2">{{ __('Action') }}</th>
-                    </tr>
-                </thead>
-                <tbody align="center">
-                    @foreach ($products as $product)
-                        <tr>
-                            <td>
-                                {{-- <a href="/products/{{ $product->id }}"> --}}
-                                {{-- <a href="{{ route('products.show', $product->id) }}">
-                                    {{ $product->name }}
-                                </a>
-                            </td>
-                            <td>{{ $product->price }}</td>
-                            <td>{{ $product->weight }}</td>
-                            <td>{{ $product->quantity }}</td>
-                            <td>{{ $product->user->name }}</td>
-                            <td>
-                                <a href="{{ route('products.edit', $product) }}">
-                                    {{ __('Edit') }}
-                                </a>
-                            </td>
-                            <td>
-                                <form action="{{ route('products.destroy', $product->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
 
-                                    <button type="submit">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @endif
-    </div> --}} --}}
 @endsection
